fde10305e56ae678ce9c69ae56961969880a1451
kle-guen <leguen.kln@gmail.com>
2024-09-25 19:22:51 +0200

    add page-layout/user-card/form

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {PageLayoutComponent} from "./components/page-layout/page-layout.component";
+import {HomeComponent} from "./components/home/home.component";
+import {LandingComponent} from "./components/landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: LandingComponent,
+	},
+	{
+		path: 'home',
+		component: PageLayoutComponent,
+		// canActivate: [AuthGuard],
+		children: [
+			{path: '', component: HomeComponent},
+		]
+	},
+];
