3d6113f3783c35958ee2aa063b0cb0b540c1b1de
kle-guen <leguen.kln@gmail.com>
2024-10-16 19:28:59 +0200

    add resolver update-profile / update-profile

--- Frontend/src/app/data/http/profile-http.service.ts
+++ Frontend/src/app/data/http/profile-http.service.ts
@@ -1,5 +1,7 @@
 import {inject, Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {ProfileDto} from "../dto/receive/profile.dto";
 
 @Injectable({
 	providedIn: "root"
@@ -11,5 +13,27 @@ export class ProfileHttpService {
 	 */
 	private readonly http = inject(HttpClient)
 
-
+	getProfile(): Observable<ProfileDto> {
+		return of(
+			{
+				name: 'John Doe',
+				firstName: 'John',
+				username: 'johndoe',
+				email: 'johndoe@gmail.com',
+				birthDate: new Date('1996-01-01'),
+				profilePicture: 'https://www.google.com',
+				firstAdditionalPicture: 'https://www.google.com',
+				secondAdditionalPicture: 'https://www.google.com',
+				thirdAdditionalPicture: 'https://www.google.com',
+				fourthAdditionalPicture: 'https://www.google.com',
+				sexualOrientation: 'Straight',
+				gender: 'Male',
+				interests: [
+					'sports'
+				],
+				description: 'I am a cool guy',
+				localisation: 'Paris'
+			} as ProfileDto
+		);
+	}
 }
